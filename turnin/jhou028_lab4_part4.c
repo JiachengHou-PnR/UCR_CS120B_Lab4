a5332fb0a75575502d33453d7b510ade9407b318
Jiacheng Hou <hou.jiacheng.w@gmail.com>
2020-04-19 03:21:21 -0700

    "Completed part 4"

---
+++ turnin/jhou028_lab4_part4.c
@@ -0,0 +1,131 @@
+/*	Author: houjiacheng
+ *	Lab Section: 022
+ *	Assignment: Lab 4  Exercise 1
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+enum States {Start, locked, step1, step2, unlocked} state;
+
+unsigned char tmpA0, tmpA1, tmpA2, tmpA7;
+unsigned char tmpB;
+
+void Tick() {
+
+    switch (state)      // Transitions
+    {
+    case Start:
+        state = locked;
+        break;
+
+    case locked:
+        tmpB = 0x00;
+        if (tmpA2 && !(tmpA0 || tmpA1 || tmpA7))
+        {
+            state = step1;
+        }
+        else // (other / multiple keys)
+        {
+            state = locked;
+        }
+        break;
+
+    case step1:
+        if (tmpA2 && !(tmpA0 || tmpA1 || tmpA7))
+        {
+            state = step1;
+        }
+        else if (!tmpA2 && !(tmpA0 || tmpA1 || tmpA7))
+        {
+            state = step2;
+        }
+        else // (other / multiple keys)
+        {
+            state = locked;
+        }
+        break; 
+
+    case step2:
+        if (!tmpA2 && !(tmpA0 || tmpA1 || tmpA7))
+        {
+            state = step2;
+        }
+        else if (tmpA1 && !(tmpA0 || tmpA2 || tmpA7))
+        {
+            state = unlocked;
+        }
+        else // (other / multiple keys)
+        {
+            state = locked;
+        }
+        break; 
+        
+    case unlocked:
+        if (tmpA7 && !(tmpA0 || tmpA1 || tmpA2))
+        {
+            state = locked;
+        }
+        else // (other / multiple keys)
+        {
+            state = unlocked;
+        }
+        break; 
+
+    default:
+        state = locked;
+        break;
+    }
+
+    switch (state)      // State Actions
+    {
+    case Start:     break;
+    
+    case locked:    
+        tmpB = 0x00;
+        break;
+
+    case step1:     break;
+
+    case step2:     break;
+    
+    case unlocked:  
+        tmpB = 0x01;
+        break;
+
+    default:        break;
+    }
+}
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    // Inputs
+    DDRA = 0x00; PORTA = 0xFF;
+
+    // Outputs
+    DDRB = 0xFF; PORTB = 0x00;
+    DDRC = 0xFF; PORTC = 0x00;
+
+    /* Insert your solution below */
+    tmpB = 0x00;
+    state = Start;
+
+    while (1) {
+        tmpA0 =  PINA & 0x01;
+        tmpA1 = (PINA >> 1) & 0x01;
+        tmpA2 = (PINA >> 2) & 0x01;
+        tmpA7 = (PINA >> 7) & 0x01;
+
+        Tick();
+
+        PORTB = tmpB;
+        PORTC = state;
+    }
+    
+    return 1;
+}
